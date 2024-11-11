a65ca2f1e0e407134b04d4d4fba7f6f06c6d1ab7
BFGprog <karpov0005@gmail.com>
2024-11-11 21:20:34 +0300

    add Bean BuildProperties

---
+++ src/main/java/sky/pro/bankstar/configuration/ConfigurationBuildProperties.java
@@ -0,0 +1,17 @@
+package sky.pro.bankstar.configuration;
+
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
+public class ConfigurationBuildProperties {
+
+    @Bean
+    public BuildProperties buildProperties() {
+        return new BuildProperties(new Properties());
+    }
+
+}
