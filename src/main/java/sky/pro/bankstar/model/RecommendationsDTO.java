64ff19ce10ba486f75f45cb1132f47dd48ec98cc
BFGprog <karpov0005@gmail.com>
2024-11-26 22:14:14 +0300

    added stats

---
+++ src/main/java/sky/pro/bankstar/model/RecommendationsDTO.java
@@ -0,0 +1,19 @@
+package sky.pro.bankstar.model;
+
+import java.util.List;
+import java.util.UUID;
+
+public class RecommendationsDTO {
+
+    private Long id;
+    private String product_name;
+    private UUID product_id;
+    private String product_text;
+    private List<Rule> rule;
+
+
+
+
+
+
+}
---
+++ src/test/java/sky/pro/bankstar/BankStarApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.bankstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankStarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
